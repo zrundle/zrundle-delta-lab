8937f5de6a2b35322ebf0991d73b1980ab2c7c9c
zrundle <157517004+zrundle@users.noreply.github.com>
2024-02-14 23:37:59 -0600

    COMMIT

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,7 +1,5 @@
 $(function() {
-    console.log("doc is ready");
-
-    let userName; 
+    console.log("doc is ready"); 
 
     $("#btnGetFacts").click(function () {
     
@@ -15,15 +13,15 @@ $(function() {
         //print back the result
         if(userName.length < 5){
 
-            alert("Less than 5");
+            alert("Short and Sweet! We love it!");
             var lastLetter = userName.slice(-1); // Get the last letter
             var letterToFront = lastLetter + userName.slice(0, -1); // Move the last letter to the front
             $('#userName').val(letterToFront); // Set the updated string back to the input field
-            thisString = 'This is your name, but with the last letter first ' + letterToFront;
+            thisString = `This is your name, but with the last letter first ${letterToFront}.`;
 
         } else {
 
-            alert("BOOOOOOO, I HATE LONG NAMES!");
+            alert("BOOOOOOO, WE HATE LONG NAMES!");
             for (var i = chars.length - 1; i > 0; i--) {
                 var j = Math.floor(Math.random() * (i + 1));
                 var temp = chars[i];
@@ -34,10 +32,13 @@ $(function() {
             // Convert the array of characters back to a string
             var shuffledInput = chars.join('');
             thisString = "Hello " + userName + ' here is your shuffled name: ' + shuffledInput;
+            $("#startPic").attr("src", 'https://pbs.twimg.com/media/FJyzt_rXwAMgo3q?format=jpg&name=medium')
+            
 
         }
 
 
-        $('body').append(thisString);
+        $('body').append("<p class = 'txtAdj'>" + thisString + "</p>");
+
     });  
 }); 
